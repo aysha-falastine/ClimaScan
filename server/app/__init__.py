1487737e05c94db6d57eb24f56e5a8938211b2e8
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-10-25 15:52:00 +0300

    Fixed API route structure for properties and dashboard blueprints

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,8 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import config
 from app.database.db import db
-from app.routes import register_blueprints
 from flask_cors import CORS
+
+
+from app.routes import register_blueprints
+
+
+from app.routes.dashboard import dashboard_bp
 
 
 def create_app(config_name='default'):
@@ -14,11 +19,17 @@ def create_app(config_name='default'):
     print(f"Running in '{config_name}' mode")
     print("Connected to database:", app.config["SQLALCHEMY_DATABASE_URI"])
 
+    
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+    
     db.init_app(app)
     Migrate(app, db)
 
+    
     register_blueprints(app)
+
+    
+    # app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
 
     return app
