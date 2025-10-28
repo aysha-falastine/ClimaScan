a9dd059d33ce863e72e68566289627100cacf0e0
asha mohamed <falastine924@gmail.com>
2025-10-28 17:03:49 +0300

    edited

--- client/src/app/reports/layout.js
+++ client/src/app/reports/layout.js
@@ -1,5 +1,6 @@
 import SidebarLayout from "@/components/SideBar";
 
-export default function PropertiesLayout({ children }) {
+export default function ReportsLayout({ children }) {
+  // SidebarLayout is a client component, so we can render it directly
   return <SidebarLayout>{children}</SidebarLayout>;
 }
