007366f2713ded7aa7e14fcd9c98a0bf3deafb0d
asha mohamed <falastine924@gmail.com>
2025-10-28 17:02:49 +0300

    debugged

--- server/app/services/auth_service.py
+++ server/app/services/auth_service.py
@@ -1,6 +1,5 @@
 from app.models.user import User
 from app.database.db import db
-
 def update_user_profile(user_id, data):
     user = User.query.get(user_id)
     if not user:
@@ -8,7 +7,7 @@ def update_user_profile(user_id, data):
 
     user.name = data.get("name", user.name)
     user.email = data.get("email", user.email)
-    if "password" in data:
+    if "password" in data and data["password"]:
         user.set_password(data["password"])  # assumes hashed password method
 
     user.default_location = data.get("defaultLocation", user.default_location)
@@ -16,3 +15,27 @@ def update_user_profile(user_id, data):
 
     db.session.commit()
     return user.to_dict()
+
+
+class AuthService:
+    """Compatibility wrapper expected by tests and other modules."""
+
+    @staticmethod
+    def register(email, password, name=None):
+        from app.services.auth_services import register_user
+
+        user = register_user(email, password)
+        if user and name:
+            user.name = name
+            db.session.commit()
+        return user
+
+    @staticmethod
+    def authenticate(email, password):
+        from app.services.auth_services import authenticate_user
+
+        return authenticate_user(email, password)
+
+    @staticmethod
+    def update_profile(user_id, data):
+        return update_user_profile(user_id, data)
