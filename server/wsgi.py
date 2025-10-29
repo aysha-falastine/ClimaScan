96b1b9d73cfb82010eedd537b3c546892606aef9
asha mohamed <falastine924@gmail.com>
2025-10-29 16:11:07 +0300

    updated

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,8 @@
 # wsgi.py
+import os
 from app import create_app
+from config import config
 
-app = create_app()
+# Use production config by default, or based on FLASK_ENV
+env = os.getenv('FLASK_ENV', 'production')
+app = create_app(config.get(env, config['production']))
